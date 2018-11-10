6d5597c0779645993384caff9c6af4d7b6fb4ae7
gian-estacio <2015085933@ust-ics.mygbiz.com>
2018-11-10 13:26:15 +0800

    Initial commit

---
+++ app/src/main/java/giancarlo/estacio/com/pnr_app/Screen2.java
@@ -0,0 +1,15 @@
+package giancarlo.estacio.com.pnr_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Screen2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen2);
+    }
+}
+
+
