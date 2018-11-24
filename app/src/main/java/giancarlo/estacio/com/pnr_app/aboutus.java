4f195dcf5461cee02b6e15f05d428b7af05a8020
gian-estacio <2015085933@ust-ics.mygbiz.com>
2018-11-24 15:05:58 +0800

    update

---
+++ app/src/main/java/giancarlo/estacio/com/pnr_app/aboutus.java
@@ -0,0 +1,20 @@
+package giancarlo.estacio.com.pnr_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class aboutus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aboutus);
+    }
+
+    public void home(View v){
+        Intent i = new Intent(this,MainActivity.class);
+        startActivity(i);
+    }
+}
---
+++ app/src/main/java/giancarlo/estacio/com/pnr_app/trivia2.java
@@ -0,0 +1,21 @@
+package giancarlo.estacio.com.pnr_app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class trivia2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.trivia2);
+    }
+
+    public void display2(View v){
+        Intent i = new Intent(this,Screen2.class);
+        startActivity(i);
+    }
+
+}
